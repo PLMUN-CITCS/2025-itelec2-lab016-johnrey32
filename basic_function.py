ee3a22c0abc40d532454dc3cad3631e12bdfff8e
johnrey32 <bulfajohnrey_bsit@plmun.edu.ph>
2025-02-20 12:16:27 +0800

    Submitting Python Week 04 - Laboratory # 16

---
+++ basic_function.py
@@ -0,0 +1,11 @@
+# John Rey Bulfa
+# ITELEC2
+# Laboratory #15 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def main():
+    def greet():  # Docstring
+        print("Hello, Python Students!")  # Function body
+    greet()
+if __name__ == "__main__":
+    main()
